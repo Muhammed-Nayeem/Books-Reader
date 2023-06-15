edad1f5420f78c72a22ee528f250320f937c2018
Muhammed Nayeem <mohammednayeem808@gmail.com>
2023-06-15 15:05:23 +0600

    Footer Component Added with Year functionality

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Footer = () => {
+  const getYear = () => {
+    const year = new Date();
+    return year.getFullYear();
+  };
+
+  return (
+    <footer className="bg-white py-4">
+      <div className="container mx-auto px-4 sm:px-0">
+        <div className="text-center">
+          <p className="text-sm font-normal text-gray-500">
+            &copy; {getYear()} BooksReader Inc. All Right Reserved
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
